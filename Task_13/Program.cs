3d7809d98b3139181a48f9616c370232c455d120
Your Name <e@w.com>
2022-08-09 14:46:16 +0300

    Программа выводит третью цифру заданного числа или сообщает, что третьей цифры нет.

---
+++ Task_13/Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+int number = new Random().Next(10,1000);
+Console.WriteLine($"Случайное число: {number}");
+if (number/100 == 0)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+else
+{
+    Console.WriteLine($"Третья цифра числа {number%10}");
+}
