069165f40906055103eabe56434ba7379fb4c766
Your Name <e@w.com>
2022-08-09 16:20:35 +0300

    принимает на вход цифру, проверяет, является ли выходным Plase enter the commit message for your changes. Lines starting

---
+++ Task_15/Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа  принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+int number = new Random().Next(1,7);
+if (number <=5)
+{
+    Console.WriteLine($"{number} - нет");
+    }
+else
+{
+    Console.WriteLine($"{number} - да");
+}
+
